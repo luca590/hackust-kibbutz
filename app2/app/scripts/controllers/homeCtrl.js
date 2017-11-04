a486a8f7786ff4b9d4c94a4a1c45485aaefdfe4a
Long Hoang <long@mindworker.de>
2017-11-05 06:57:16 +0800

    Add communities page

--- app2/app/scripts/controllers/homeCtrl.js
+++ app2/app/scripts/controllers/homeCtrl.js
@@ -8,5 +8,13 @@
  * Controller of AniTheme
  */
 angular.module('AniTheme').controller('HomeCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
+
+  $scope.communities = [{
+    name: "Education in Africa",
+    desc: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum."
+  },{
+    name: "Sustainable Communes in Cambodia",
+    desc: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum."
+  }];
   
 }]);
