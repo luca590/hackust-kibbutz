1db0b0e84622cb2807278cfe5e7f846a675512ee
Long Hoang <long@mindworker.de>
2017-11-04 22:53:18 +0800

    Generalize community view

--- app/app.js
+++ app/app.js
@@ -1,3 +1,27 @@
+const Web3 = require("web3");
+const Promise = require("bluebird");
+const truffleContract = require("truffle-contract");
+
+// Not to forget our built contract
+const communityJson = require("../build/contracts/Community.json");
+
+// Supports Mist, and other wallets that provide 'web3'.
+if (typeof web3 !== 'undefined') {
+    // Use the Mist/wallet/Metamask provider.
+    console.log("Using Mist/Wallet/Metamask");
+    window.web3 = new Web3(web3.currentProvider);
+} else {
+    // Your preferred fallback.
+    console.log("Using testRPC/geth");
+    window.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+}
+
+Promise.promisifyAll(web3.eth, { suffix: "Promise" });
+Promise.promisifyAll(web3.version, { suffix: "Promise" });
+
+const Community = truffleContract(communityJson);
+Community.setProvider(web3.currentProvider);
+
 var app = angular.module('App', ["ngRoute"]);
 
 app.config(function ($routeProvider) {
@@ -8,15 +32,11 @@ app.config(function ($routeProvider) {
     })
     .when("/communities", {
       templateUrl: "communities.html",
+      controller: "CommunitiesController"
+    })
+    .when("/communities/:id", {
+      templateUrl: "community.html",
       controller: "CommunityController"
-    })
-    .when("/communities/0", {
-      templateUrl: "education.html",
-      controller: "EducationController"
-    })
-    .when("/communities/1", {
-      templateUrl: "food.html",
-      controller: "FoodController"
     })
     .when("/create_community", {
       templateUrl: "create_community.html",
@@ -33,52 +53,61 @@ app.controller("HomeController",
     $scope.test = "Hello World";
   }]);
 
-app.controller("FoodController", ['$scope', function ($scope) {
-  $scope.communities = [{}]
-}])
+app.controller("CommunityController", function ($scope, $routeParams) {
+  $scope.data = [
+    {
+      proposals:[
+	{
+	  name: "Build a high school in Kinshasa",
+	  time: "1 day",
+	  raised: 50000,
+	  goal: 80000,
+	  id: 0
+	},
+	{
+	  name: "Provide new chairs and tables to a school in Cambodia	",
+	  time: "5 hours",
+	  raised: 3000,
+	  goal: 5000,
+	  id: 1
+	},
+	{
+	  name: "Research on magnetic waves",
+	  time: "2 weeks",
+	  raised: 80000,
+	  goal: 200000,
+	  id: 2
+	},
+      ],
+      contributors: [
+	{
+          name: "Paul Jack",
+          amount: 50000
+	},
+	{
+          name: "James Ryan",
+          amount: 25000
+	},
+	{
+          name: "Anon",
+          amount: 10000
+	}
+      ],
+      icon: "http://smallbusinessbc.ca/wp-content/uploads/2015/04/icon-education.png"
+    },
+    {
+      proposals: [],
+      contributors: [],
+      icon: ""
+    }
+  ];
 
-app.controller("EducationController", ['$scope', function ($scope) {
-  $scope.proposals = [
-    {
-      name: "Build a high school in Kinshasa",
-      time: "1 day",
-      raised: 50000,
-      goal: 80000,
-      id: 0
-    },
-    {
-      name: "Provide new chairs and tables to a school in Cambodia	",
-      time: "5 hours",
-      raised: 3000,
-      goal: 5000,
-      id: 1
-    },
-    {
-      name: "Research on magnetic waves",
-      time: "2 weeks",
-      raised: 80000,
-      goal: 200000,
-      id: 2
-    },
-  ]
-  $scope.contributors = [
-      {
-        name: "Paul Jack",
-        amount: 50000
-      },
-      {
-        name: "James Ryan",
-        amount: 25000
-      },
-      {
-        name: "Anon",
-        amount: 10000
-      }
-    ]
-  $scope.icon = "http://smallbusinessbc.ca/wp-content/uploads/2015/04/icon-education.png"
-}])
+  let cid = $routeParams.id;
+  $scope.proposals = $scope.data[cid].proposals;
+  $scope.contributors = $scope.data[cid].contributors;
+})
 
-app.controller("CommunityController", ['$scope', function ($scope) {
+app.controller("CommunitiesController", ['$scope', function ($scope) {
   $scope.communities = [{
     name: "Education",
     member_amount: 20000,
@@ -91,3 +120,35 @@ app.controller("CommunityController", ['$scope', function ($scope) {
 }]);
 
 app.controller("CreateCommunityController", ['$scope', function ($scope) { }]);
+
+app.controller("ProjectsController", [ '$scope', function($scope) {
+    $scope.community = "Test Community";
+    $scope.projects = [{
+	name: "Build hospital",
+	description: "Test 123",
+	timeline: "2 years",
+	budget: "1500 ETH",
+	status: "VOTING"
+    }, {
+	name: "SOS Kinderdorf",
+	description: "Test 123",
+	timeline: "6 months",
+	budget: "100 ETH",
+	status: "APPROVED"
+    }];
+
+    var contract;
+    Community.deployed().then(instance => {
+        contract = instance;
+        //$scope.address = contract.address;
+    });
+
+    $scope.getProjects = function() {
+	console.log("klicked!");
+	contract.proposals.call(0)
+	    .then(proposals => {
+		console.log(proposals);
+	    });
+    }
+
+}]);
--- webpack.config.js
+++ webpack.config.js
@@ -13,9 +13,9 @@ module.exports = {
       { from: './app/index.html', to: "index.html" },
       { from: './app/home.html', to: "home.html" },
       { from: './app/communities.html', to: "communities.html" },
+      { from: './app/community.html', to: "community.html" },
       { from: './app/create_community.html', to: "create_community.html" },
-      { from: './app/education.html', to: "education.html"},
-      { from: './app/food.html', to: "food.html"},
+      { from: './app/projects.html', to: "projects.html" },
     ])
   ],
   module: {
